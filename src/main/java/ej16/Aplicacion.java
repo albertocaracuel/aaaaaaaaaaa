04bcac6e010311c9850f11dbe884079e6b244cd1
alberto <albertocaracuel16@gmail.com>
2021-03-18 11:00:46 +0100

    modularizacion

--- src/main/java/ej16/Aplicacion.java
+++ src/main/java/ej16/Aplicacion.java
@@ -15,43 +15,57 @@ import java.util.Comparator;
  */
 public class Aplicacion {
 
-    public static void main(String[] args) {
-        //crear lista con 20 robots aleatorios
-        ArrayList<Robots> lista = new ArrayList<Robots>();
+    //crear lista con 20 robots aleatorios
+    private ArrayList<Robots> lista = new ArrayList<Robots>();
+    private ArrayList<Robots> lista2 = new ArrayList<Robots>();
 
+    public Aplicacion() {
+    }
+
+    
+    public void crearLista() {
         for (int i = 0; i < 20; i++) {
-            lista.add(new Robots());
+            this.lista.add(new Robots());
 
         }
-        lista.forEach(System.out::println);
+    }
 
-        System.out.println("-----------------------------------------");
-        //ordenar e imprimir de mayor a menor vida
-
+    //ordenar e imprimir de mayor a menor vida
+    public void mayorMenorVida() {
         Collections.sort(lista, (v1, v2) -> v2.getVida().compareTo(v1.getVida()));
         lista.forEach(System.out::println);
         System.out.println("-----------------------------------------");
-        //ordenar e imprimir menor a mayor vida
+    }
+    //ordenar e imprimir menor a mayor vida
 
+    public void menorMayorVida() {
         Collections.sort(lista, (v1, v2) -> v1.getVida().compareTo(v2.getVida()));
         lista.forEach(System.out::println);
-        System.out.println("-----------------------------------------");
+    }
 
-        //imprimir mayores de 50%
-        ArrayList<Robots> lista2 = new ArrayList<>();
+    //imprimir mayores de 50%
+    public void mayores50deVida() {
         for (Robots a : lista) {
             if (a.getVida() > 50) {
-                lista2.add(a);
+                this.lista2.add(a);
             }
 
         }
-        //imprimir lista2
-        lista2.forEach(System.out::println);
-        System.out.println("-----------------------------------------");
 
-        //ordenar por numero de serie
-        Collections.sort(lista, (v1, v2) -> v1.getNumeroSerie().compareTo(v2.getNumeroSerie()));
-        
-        //busqueda binaria 
     }
+    //imprimir lista2
+
+    public void imprimirLista2() {
+        this.lista2.forEach(System.out::println);
+    }
+
+    //ordenar por numero de serie
+    public void ordenarPorSerie() {
+        Comparator<Robots> criterioserie = (v1, v2) -> v1.getNumeroSerie().compareTo(v2.getNumeroSerie());
+        Comparator<Robots> criteriovida = (v1, v2) -> v1.getVida().compareTo(v2.getVida());
+
+        Collections.sort(this.lista2, criterioserie.reversed().thenComparing(criteriovida));
+    }
+
+    //busqueda binaria 
 }
